37266d8465fc41994fadb5c3de973c413af03223
Romulo Pransteter <romulo.silva@printi.com.br>
2018-03-29 18:05:02 -0300

    Routes engine is working!

---
+++ project/src/Controller/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserController extends Controller
+{
+    /**
+     *
+     * @Method({"GET"})
+     * @Route("/user")
+     */
+    public function getAll()
+    {
+        $userList = [
+            0 => (object) [
+                'id'        => 1,
+                'firstName' => 'Romulo',
+                'lastName'  => 'Pransteter',
+                'gender'    => 'Male',
+                'age'       => '30',
+                'email'     => 'r.pransteter@gmail.com',
+            ],
+            1 => (object) [
+                'id'        => 2,
+                'firstName' => 'Afonso',
+                'lastName'  => 'Pransteter',
+                'gender'    => 'Male',
+                'age'       => '30',
+                'email'     => 'r.pransteter@gmail.com',
+            ],
+            2 => (object) [
+                'id'        => 3,
+                'firstName' => 'Lineu',
+                'lastName'  => 'Pransteter',
+                'gender'    => 'Male',
+                'age'       => '30',
+                'email'     => 'r.pransteter@gmail.com',
+            ],
+            3 => (object) [
+                'id'        => 4,
+                'firstName' => 'Golias',
+                'lastName'  => 'Pransteter',
+                'gender'    => 'Male',
+                'age'       => '30',
+                'email'     => 'r.pransteter@gmail.com',
+            ],
+            4 => (object) [
+                'id'        => 5,
+                'firstName' => 'Ulisses',
+                'lastName'  => 'Pransteter',
+                'gender'    => 'Male',
+                'age'       => '30',
+                'email'     => 'r.pransteter@gmail.com',
+            ],
+        ];
+
+        return new JsonResponse($userList);
+    }
+}
